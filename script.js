912f13084be79f19f98d9966004aae9d64b70b61
Mohamed Hatem <113688819+MohHat@users.noreply.github.com>
2022-09-23 00:25:14 +0000

    Fixed all the errors

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ $(window).on('scroll', function () {
 });
 */
 const nav = document.querySelector('nav');
-const header = document.querySelector('header')
-
+const header = document.querySelector("#website_head")
 const options = {
   rootMargin: '-12%'
 }
@@ -18,12 +17,33 @@ const observer = new IntersectionObserver(entries => {
     if(entry.isIntersecting)
     {
       nav.classList.remove('changeColor')
-      
+            nav.classList.add('onHover')
+
+
     }else{
             nav.classList.add('changeColor')
-           
+                  nav.classList.remove('onHover')
+
     }
   })
 }, options)
 
 observer.observe(header)
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
